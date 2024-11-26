b644cf1bc127b92cd4fff9073c6bf26518cebd02
mouadispunk <bencaidmouad9@gmail.com>
2024-11-26 06:01:44 +0100

    created some components, no logic code yet, next step is to configure routes and the routing strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { StudentComponent } from './components/student/student.component';
+import { DemandeComponent } from './components/student/demande/demande.component';
+import { ReclamationComponent } from './components/student/reclamation/reclamation.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AdminLoginComponent } from './components/admin-login/admin-login.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    // component for the Landing Page
+    LandingPageComponent,
+    // components for the Student Template
+    StudentComponent,
+    DemandeComponent,
+    ReclamationComponent,
+    // components for the Admin Login Page
+    AdminLoginComponent,
+    // components for the Admin
+    AdminComponent
+    
+
     // other components
   ],
   imports: [
---
+++ src/app/components/student/reclamation/reclamation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reclamation',
+  standalone: false,
+  templateUrl: './reclamation.component.html',
+  styleUrl: './reclamation.component.css'
+})
+export class ReclamationComponent {
+
+}
