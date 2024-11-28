c708866c2abd48c413033da877b32c7f7057c8a9
mouadispunk <bencaidmouad9@gmail.com>
2024-11-28 07:31:29 +0100

    created the models and enums and examples in db.json
    a model called demande that has the necessary fields for submission and display and for the backend operations (more on that later on), a model called reclamation that has the necessary fields (i dont know if we should relate a demande to a reclamation) , created necessary enums for type and status , i populated the db.json with some examples

---
+++ src/app/models/enums/document-status.ts
@@ -0,0 +1,5 @@
+export enum DemandeStatus {
+    Pending = 'Pending', // Default status when demande is created
+    Validated = 'Validated', 
+    Refused = 'Refused' 
+}
---
+++ src/app/models/enums/document-type.ts
@@ -0,0 +1,4 @@
+export enum DocumentType {  // each value of this enum is a string
+    Attestation = 'Attestation de scolarité',
+    ReleveNotes = 'Relevé de notes',
+}
